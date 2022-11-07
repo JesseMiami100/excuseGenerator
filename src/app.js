0229a0819b5c1524582d5d207978021899e621c2
JesseMiami100 <jessemetayer@gmail.com>
2022-11-07 01:06:43 +0000

    excuseGenerator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The cat", "My abeulo", "His snake", "My parate", " the police"];
+  let action = ["ate", "peed", "lost", "broke"];
+  let what = ["my passport", "the cheese", "the vase"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  function getRandomNumber(n) {
+    return Math.floor(Math.random() * n);
+  }
+  let element = document.getElementById("excuse");
+  let button1 = document.getElementById("button-1");
+  function generateExcuse() {
+    let excuse =
+      who[getRandomNumber(who.length)] +
+      " " +
+      action[getRandomNumber(action.length)] +
+      " " +
+      what[getRandomNumber(what.length)] +
+      " " +
+      when[getRandomNumber(when.length)];
+    element.innerHTML = excuse;
+  }
+  generateExcuse();
+  button1.addEventListener("click", generateExcuse);
 };
